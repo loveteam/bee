8484909f3bf8ff96886bf2382fc4ffc544ebe44a
Guangshan <guangshan1992@qq.com>
2015-07-15 00:23:50 +0800

    add argular.js menu.js

---
+++ public/js/indexMenu.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Wangyao on 2015/7/14.
+ */
+var indexMenuApp = angular.module("indexMenu", []);
+indexMenuApp.controller("menuController", function($scope, $http) {
+    $scope.menus = [
+        {"name":"Home","url":""},
+        {"name":"Article","url":""},
+        {"name":"Post","url":""},
+        {"name":"Comment","url":""},
+        {"name":"Theme","url":""},
+        {"name":"Configure","url":""}];
+    $scope.menuClick = function(name) {
+        var url = "/" + name;
+        $scope.activeMenuContent = url;
+        $http.post(url, {}).success(function(response) {
+            $scope.activeMenuContent = response;
+            //$scope.menu[name] = response;
+        });
+    };
+});
