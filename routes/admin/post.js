48141cf14bd516308c28dca61b32a72b46c317f4
yaolilceng <yaoliceng@ruishengtech.com>
2015-07-14 17:23:22 +0800

    Dao

---
+++ routes/admin/post.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+    res.render('admin/post');
+});
+
+router.post('/save',function(req,res){
+
+});
+
+module.exports = router;
+
+
