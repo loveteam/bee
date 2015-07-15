cee32dcffa8d90a1c44d9f3676e26a2047cc2166
yaolilceng <yaoliceng@ruishengtech.com>
2015-07-15 14:05:58 +0800

    有点问题的dao

--- routes/admin/post.js
+++ routes/admin/post.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var dao =new require("../../lib/core/dao")
 
 router.get('/', function(req, res, next) {
     res.render('admin/post');
 });
 
-router.post('/save',function(req,res){
-
+router.post('/addPost',function(req,res){
+    dao.save({d:"1"},function(err,newOb){
+        console.log(err)
+        res.render('admin/index');
+    })
 });
 
 module.exports = router;
