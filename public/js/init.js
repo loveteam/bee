f10a2d5ad9c5cbfd92f9761f4a0c63a79f975083
guangshan <guangshan1992@163.com>
2015-07-15 20:01:12 +0800

    add init.js

---
+++ public/js/init.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Wangyao on 2015/7/15.
+ */
+
+//semantic命名空间，只用这一个全局变量
+window.semantic = {
+    handler: {}
+};
+
+semantic.ready = function() {
+
+};
+
+$(document).ready(semantic.ready);
--- routes/admin/post.js
+++ routes/admin/post.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-var Post = require("../../lib/service/post")
-var post = new Post()
+var Post = require("../../lib/service/post");
+var post = new Post();
 
 router.get('/', function(req, res, next) {
+    res.render('admin/post');
+});
+
+router.post('/', function(req, res, next) {
     res.render('admin/post');
 });
 
 router.post('/addPost',function(req,res){
     post.save({name:req.body.name},function(err,newOb){
         res.render('admin/index');
-    })
+    });
 });
 
 module.exports = router;
